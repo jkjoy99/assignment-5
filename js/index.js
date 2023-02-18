8de4bbc4fa3b9b436fb88d8f235c60d103ac4b3a
jkjoy99 <jkjoy99@gmail.com>
2023-02-18 14:22:38 -0800

    plm solv someting html and javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+
+function displayData (name, totle) {
+    const container = document.getElementById('table-container');
+    const tr =document.createElement('tr');
+    tr.innerHTML = `
+        <td>${1.}</td>
+        <td>${name}</td>
+        <td>${'Total'}</td>
+        <td>${totle}</td>
+    `;
+    container.appendChild(tr);
+   
+}
+
+
+
+//----------------
+//  triangle card
+
 document.getElementById('btn-Triangle').addEventListener('click', function(){
     const triangleElement = document.getElementById('Triangle').innerText;
     const triangleInfutfild1 = document.getElementById('Triangle-input-b');
@@ -5,16 +24,28 @@ document.getElementById('btn-Triangle').addEventListener('click', function(){
     const triangleInfutfild2 = document.getElementById('Triangle-input-h');
     const newTriangleInfutfild2 = triangleInfutfild2.value;
     const triangleInfutTotal = 0.5 * newTriangleInfutfild1 * newTriangleInfutfild2;
-    const container = document.getElementById('table-container');
-    const tr =document.createElement('tr');
-    tr.innerHTML = `
-        <td>${1.}</td>
-        <td>${triangleElement}</td>
-        <td>${'Total'}</td>
-        <td>${triangleInfutTotal}</td>
-    `;
-    container.appendChild(tr);
     triangleInfutfild1.value = '';
-    triangleInfutfild2.value = '';
+    triangleInfutfild2.value = ''; 
+    displayData(triangleElement, triangleInfutTotal)
     
-})
+});
+
+// ---------------
+// rectangle card
+
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    const rectangleElement = document.getElementById('rectangle').innerText;
+    const rectangleInputfild1 =document.getElementById('rectangle-input-w');
+    const newRectangleInfutfild1 = rectangleInputfild1.value;
+    const rectangleInputfild2 =document.getElementById('rectangle-input-i');
+    const newRectangleInfutfild2 =rectangleInputfild2.value;
+    const rectangleTotle = newRectangleInfutfild1 * newRectangleInfutfild2;
+    rectangleInputfild1.value = '';
+    rectangleInputfild2.value = '';
+
+    displayData(rectangleElement, rectangleTotle);
+});
+
+// ----------------------
+// Parallelogram card
+
